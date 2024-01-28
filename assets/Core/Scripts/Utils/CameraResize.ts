afdd0b325683950260bf87aab307e650e252c68b
gamedeve <duos0770@gmail.com>
2024-01-28 21:17:00 +0700

    разобрался с дисплеем

---
+++ assets/Core/Scripts/Utils/CameraResize.ts
@@ -0,0 +1,15 @@
+import { _decorator, Component, Node, Widget } from 'cc';
+const { ccclass, property } = _decorator;
+
+@ccclass('CameraResize')
+export class CameraResize extends Component {
+    start() {
+
+    }
+
+    update(deltaTime: number) {
+        
+    }
+}
+
+
